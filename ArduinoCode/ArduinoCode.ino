798f7a535e1465ca8798f96f491211ee4f80a6f9
JCDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-02-27 22:00:52 +0800

    refctor code for semi final testing

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -2,12 +2,12 @@
 #include <ArduinoJson.h>
 SoftwareSerial mySerial(2,3); //RX TX
 
-int id =0;
+int id = 0;
 boolean isMobileApp = false;
 int cupsOfRiceSelected = 0;
 int riceTypeSelected = 0;
-double scaleOfRiceWater = 0;
-    
+double scaleOfRiceWater = 0.00;
+  
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
@@ -15,49 +15,54 @@ void setup() {
 }
 
 void loop() {
-  //temporary must be getHttpData()
-  String payloadString = "";
-  payloadString = String("[{\"Id\":1,\"IsMobileApp\":true,\"CupsOfRice\":5,\"RiceType\":1,\"IsDoneCooking\":false,\"IsRiceLevelLow\":false,\"IsWaterLevelLow\":false,\"RiceDistance\":\"0\",\"WaterDistance\":\"0\",\"RiceWaterScale\":\"0\"}]");
-  separateKeyValueJSONPairs(payloadString);
-  delay(2000);
-}
+  getHttpData();
 
-void getHttpData(){
-  String payloadString = "";
-  if(mySerial.available()){
-    payloadString =  mySerial.readString();
-
-    if(payloadString.length() > 1){
-      separateKeyValueJSONPairs(payloadString);
-      delay(2000);
-    }
+  if(id > 0){
+      postHttpData();
   }
 }
 
-void separateKeyValueJSONPairs(String payload){
-    StaticJsonDocument<1000> doc;
-
-    DeserializationError err = deserializeJson(doc, payload);
-
-    if (err) {
-      Serial.print(F("deserializeJson() failed with code "));
-      Serial.println(err.c_str());
-      return;
+void getHttpData(){
+    String payloadString = "";
+    if(mySerial.available()){
+      payloadString =  mySerial.readString();
+      
+      if(payloadString.length() > 1){
+        separateKeyValueJSONPairs(payloadString);
+        delay(2000);
+      }
     }
+  }
+  
+void postHttpData(){
+  String payloadString = "";
 
-    id = doc[0]["Id"];
-    isMobileApp = doc[0]["IsMobileApp"];
-    cupsOfRiceSelected = doc[0]["CupsOfRice"];
-    riceTypeSelected = doc[0]["RiceType"];
-    scaleOfRiceWater = doc[0]["RiceWaterScale"];
+  String firstParam = "\"Id\":" + String(id);
+  String secondParam = ",\"IsDoneCooking\":" + String(1);
+  String thirdParam = ",\"IsRiceLevelLow\":" + String(false);
+  String fourthParam = ",\"IsWaterLevelLow\":" + String(false);
+  String fifthParam = ",\"RiceDistance\":" + String(5.00);
+  String sixthParam = ",\"WaterDistance\":" + String(5.00);
 
-    Serial.println("Id: " + String(id));
-    delay(3000);
-    Serial.println("isMobileApp: " + String(isMobileApp));
-    delay(3000);
-    Serial.println("cupsOfRiceSelected: " + String(cupsOfRiceSelected));
-    delay(3000);
-    Serial.println("riceTypeSelected: " + String(riceTypeSelected));
-    delay(3000);
-    Serial.println("scaleOfRiceWater: " + String(scaleOfRiceWater));
+  payloadString = String("[{"+ firstParam +  secondParam + thirdParam + fourthParam + fifthParam + sixthParam + "}]");
+
+  mySerial.print(payloadString);
+}
+
+void separateKeyValueJSONPairs(String payload){
+  StaticJsonDocument<1000> doc;
+
+  DeserializationError err = deserializeJson(doc, payload);
+
+  if (err) {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(err.c_str());
+    return;
+  }
+
+  id = doc[0]["Id"];
+  isMobileApp = doc[0]["IsMobileApp"];
+  cupsOfRiceSelected = doc[0]["CupsOfRice"];
+  riceTypeSelected = doc[0]["RiceType"];
+  scaleOfRiceWater = doc[0]["RiceWaterScale"];
 }
--- ESP8266Code/ESP8266Code.ino
+++ ESP8266Code/ESP8266Code.ino
@@ -1,52 +1,33 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include<SoftwareSerial.h>
+#include <ArduinoJson.h>
+//SoftwareSerial Serial2(7,2);
 
 const char* ssid     = "JCGDeGuzman";
 const char* password = "JCGDeGuzman@15";
 
-int id;
-boolean isDoneCooking;
-boolean isRiceLevelLow;
-boolean isWaterLevelLow;
-double riceDistance;
-double waterDistance;
-boolean isMobileDataReceived;
-
+int id = 0;
+int isDoneCooking = 0;
+boolean isRiceLevelLow = 0;
+boolean isWaterLevelLow = 0;
+double riceDistance = 0.00;
+double waterDistance = 0.00;
+  
 const char* view_host = "http://restapiarc.herokuapp.com/api/configurations.php";
 
-const String firstUrl = "id="+String(id);
-const String secondUrl = "&is_done_cooking="+String(isDoneCooking);
-const String thirdUrl = "&is_rice_level_low="+String(isRiceLevelLow);
-const String fourthUrl = "&is_water_level_low="+String(isWaterLevelLow);
-const String fifthUrl = "&rice_distance="+String(riceDistance);
-const String sixthUrl = "&water_distance="+String(waterDistance);
+String firstUrl = "id="+String(id);
+String secondUrl = "&is_done_cooking="+String(isDoneCooking);
+String thirdUrl = "&is_rice_level_low="+String(isRiceLevelLow);
+String fourthUrl = "&is_water_level_low="+String(isWaterLevelLow);
+String fifthUrl = "&rice_distance="+String(riceDistance);
+String sixthUrl = "&water_distance="+String(waterDistance);
 
-const String update_url = "http://restapiarc.herokuapp.com/api/update-configuration.php?"+firstUrl+secondUrl+thirdUrl+fourthUrl+fifthUrl+sixthUrl;
+String update_url = "http://restapiarc.herokuapp.com/api/update-configuration.php?"+firstUrl+secondUrl+thirdUrl+fourthUrl+fifthUrl+sixthUrl;
 
 void setup(){
-  initializedConnection();
-  loadDefaultValues();
-}
-
-void loop() {
-  if(WiFi.status() == WL_CONNECTED){  
-      getHttpData();
-  }
-  delay(30000);
-}
-
-void loadDefaultValues(){
-  id = 1;
-  isDoneCooking = false;
-  isRiceLevelLow = false;
-  isWaterLevelLow = false;
-  riceDistance = 0;
-  waterDistance = 0;
-  isMobileDataReceived = false;
-}
-
-void initializedConnection(){
   Serial.begin(9600);
+
   delay(500);
 
   WiFi.begin(ssid, password);
@@ -56,7 +37,15 @@ void initializedConnection(){
   }
 }
 
-void getHttpData(){
+void loop() {
+  if(WiFi.status() == WL_CONNECTED){  
+      getHttpData();
+      getArduinoData();
+  }
+  delay(30000);
+}
+
+void getHttpData(){ 
   HTTPClient http;
   int httpCode = 0;
   String payload = "";
@@ -64,10 +53,61 @@ void getHttpData(){
   http.begin(view_host);
   httpCode = http.GET();
 
-  if (httpCode > 0) {
+  if (httpCode > 0){
     payload = http.getString();
     Serial.print(payload);
   }
 
   http.end();
+}
+
+void postHttpData(){
+  HTTPClient http;
+  int httpCode = 0;
+  String payload = "";
+
+  http.begin(update_url);
+  httpCode = http.GET();
+
+  if (httpCode > 0) {
+    payload = http.getString();
+  }
+
+  http.end();
+}
+
+void getArduinoData(){
+  String payloadString = "";
+
+  if(Serial.available()){
+    payloadString =  Serial.readString();
+
+    if(payloadString.length() > 1){
+      separateKeyValueJSONPairs(payloadString);
+      delay(2000);
+    }
+  }
+}
+
+void separateKeyValueJSONPairs(String payload){
+  StaticJsonDocument<1000> doc;
+
+  DeserializationError err = deserializeJson(doc, payload);
+
+  if (err) {
+    Serial.print(F("deserializeJson() failed with esp code "));
+    Serial.println(err.c_str());
+    return;
+  }
+  
+  id = doc[0]["Id"];
+  isDoneCooking = doc[0]["IsDoneCooking"];
+  isRiceLevelLow = doc[0]["IsRiceLevelLow"];
+  isWaterLevelLow = doc[0]["IsWaterLevelLow"];
+  riceDistance = doc[0]["RiceDistance"];
+  waterDistance = doc[0]["WaterDistance"];
+
+  if(id > 0){
+    postHttpData();
+  }
 }
